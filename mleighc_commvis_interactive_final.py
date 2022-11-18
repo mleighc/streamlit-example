637d4784fb10dee1be46d94a6d211cb7ec552b85
Miranda Cooper <mleighc@umich.edu>
2022-11-18 18:38:23 -0500

    edit spacing

--- mleighc_commvis_interactive_final.py
+++ mleighc_commvis_interactive_final.py
@@ -3,7 +3,7 @@ import altair as alt
 import pandas as pd
 from vega_datasets import data
 
-##DEPLOYED: 
+##DEPLOYED:
 #This app is deployed at the following link: https://mleighc-streamlit-exam-mleighc-commvis-interactive-final-8n48c7.streamlit.app/
 
 #enable theme
